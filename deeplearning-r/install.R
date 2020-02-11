39dd394ed8f794eb0d02f27dcae572d4ed99b24c
Brock Tibert <btibert3@gmail.com>
2020-02-11 12:20:57 -0500

    additional packages in r tensorflow

--- deeplearning-r/install.R
+++ deeplearning-r/install.R
@@ -1,7 +1,8 @@
 # only fail if needed
 options(warn=2) 
 # what do we want
-P = c("dplyr", "tidyr", "stringr", "lubridate", "keras", "tfdatasets")
+P = c("dplyr", "tidyr", "stringr", "lubridate", "keras", 
+      "tfdatasets", "plumber", "ggplot2")
 # install it
 install.packages(P)
 
