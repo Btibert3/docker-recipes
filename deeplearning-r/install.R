c2bf080d39dc191c9f054c6f3cca0ea4423fccfa
Brock Tibert <203343+Btibert3@users.noreply.github.com>
2020-02-12 12:50:33 -0500

    Update install.R

--- deeplearning-r/install.R
+++ deeplearning-r/install.R
@@ -2,7 +2,7 @@
 options(warn=2) 
 # what do we want
 P = c("dplyr", "tidyr", "stringr", "lubridate", "keras", 
-      "tfdatasets", "plumber", "ggplot2", "readr", "rsample")
+      "tfdatasets", "plumber", "ggplot2", "readr", "rsample", "devtools")
 # install it
 install.packages(P)
 
